fe2789bc42127058ff696c99f6c7c53e17e50ff2
emildsn <emildsn00@gmail.com>
2023-05-29 11:59:32 -0300

    Initial Commit

---
+++ example-state-ios/example_state_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  example_state_iosApp.swift
+//  example-state-ios
+//
+//  Created by Emiliano Diaz on 29/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct example_state_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
